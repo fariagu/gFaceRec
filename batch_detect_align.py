f64d2f3a413ccc381b47aa46d5a6145bb7d45db3
Gustavo Faria <gustavoduartefaria@gmail.com>
2019-04-02 16:00:43 +0100

    ready to test batch_detect_align

--- batch_detect_align.py
+++ batch_detect_align.py
@@ -1,2 +1,10 @@
-import detect_align aligned_face_0.jpg
+import os
 
+import detect_align
+import utils
+
+if not os.path.exists(utils.crop_dir):
+    os.makedirs(utils.crop_dir)
+
+for file in os.listdir(utils.images_dir):
+    detect_align.main(utils.images_dir + file, file)

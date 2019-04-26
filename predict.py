52ff8770890f6e411dec75ecdb7a055c57014b72
Gustavo Faria <gustavoduartefaria@gmail.com>
2019-04-26 18:09:12 +0100

    predict.py returning bbox on top of image using a keras model (tflite incomming)

--- predict.py
+++ predict.py
@@ -4,6 +4,14 @@ import os
 
 import tensorflow as tf
 
+from skimage.io import imread
+from skimage.transform import resize
+from skimage import data
+from skimage.viewer import ImageViewer
+import yolo_v3_interpreter as yolo
+
+import cv2
+
 import numpy as np
 import keras
 from keras.models import load_model
@@ -11,12 +19,13 @@ from keras.models import load_model
 import utils
 from load_celeba import load_test_data
 from generator import Generator
-from load_local_model import load_local_model, load_facenet_fv
+from load_local_model import load_local_model, load_facenet_fv, load_face_detector
 
 test_images, test_labels = load_test_data()
 test_batch_generator = Generator(test_images, test_labels, utils.batch_size)
 
-model = load_facenet_fv()
+# model = load_facenet_fv()
+model = load_face_detector()
 
 # # caga nisto para já. quero ver como é só com o feature vector
 # 
@@ -30,26 +39,56 @@ model = load_facenet_fv()
 # 
 # result = model.predict_generator(test_batch_generator, verbose=1)
 
-result = model.predict(img, verbose=1)
+model.summary()
 
-correct_guesses = 0
-for i, prediction in enumerate(result):
-    if i < 1:
-        print(prediction)
+# img = resize(imread("C:/datasets/CelebA/img_align_celeba/000001.jpg"),(416, 416))
+img = cv2.resize(cv2.imread("C:/datasets/CelebA/img_align_celeba/000007.jpg"), (416, 416))
+nimg = np.zeros((416, 416))
+nimg = cv2.normalize(img, nimg, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32FC1)
 
-    guess = np.argmax(prediction)
-    answer = test_labels[i]
+imgs = np.array([nimg])
+# print(imgs.shape)
 
-    if guess == answer:
-        correct_guesses += 1
-    else:
-        print("wrong guess")
-        print("G: " + str(prediction[guess]))
-        print("A: " + str(prediction[answer]))
+result = model.predict(imgs, verbose=1)
+res = yolo.interpret_output_yolov2(result[0], 416, 416)
 
-test_score = correct_guesses * 100 / len(test_labels)
+print(res)
+# print(result)
+# print(nimg)
 
-print(test_score)
+left = int(res[0][1] - (res[0][4] / 2))
+right = int(res[0][1] + (res[0][4] / 2))
+top = int(res[0][2] - (res[0][3] / 2))
+bottom = int(res[0][2] + (res[0][3] / 2))
+
+tl = (top, left)
+br = (bottom, right)
+
+#tmp
+cv2.rectangle(img, tl, br, (0, 0, 255), 3)
+
+cv2.imshow("predict", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# correct_guesses = 0
+# for i, prediction in enumerate(result):
+#     if i < 1:
+#         print(prediction)
+
+#     guess = np.argmax(prediction)
+#     answer = test_labels[i]
+
+#     if guess == answer:
+#         correct_guesses += 1
+#     else:
+#         print("wrong guess")
+#         print("G: " + str(prediction[guess]))
+#         print("A: " + str(prediction[answer]))
+
+# test_score = correct_guesses * 100 / len(test_labels)
+
+# print(test_score)
 
 
 # len(test_labels) ---- 100%

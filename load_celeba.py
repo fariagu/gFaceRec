9e61d8c0e5564647c44b5955dc4a5039717dabcb
Gustavo Faria <gustavoduartefaria@gmail.com>
2019-05-06 16:22:57 +0100

    fix folder name

--- load_celeba.py
+++ load_celeba.py
@@ -160,7 +160,7 @@ def load_vectors_into_disk():
 
     files_that_exist = []
     final_dict = {}
-    for file in os.listdir(raw_dir + "img_crop_25_aug_times_5/"):
+    for file in os.listdir(utils.raw_dir + "img_crop_25_aug_times_5/"):
     # for file in os.listdir(utils.images_dir):
         files_that_exist.append(file.split("_")[-1])
         final_dict[file] = -1
@@ -180,7 +180,8 @@ def load_vectors_into_disk():
     file_names = list(final_dict.keys())
     labels = list(final_dict.values())
 
-    paths = prepend_string_to_array(utils.images_dir, file_names)
+    # paths = prepend_string_to_array(utils.images_dir, file_names)
+    paths = prepend_string_to_array(utils.raw_dir + "img_crop_25_aug_times_5/", file_names)
     full_generator = Generator(paths, labels, utils.batch_size)
 
     model = load_facenet_fv() if utils.model_in_use == utils.FACENET else load_vgg_face_fv()

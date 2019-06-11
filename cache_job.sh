6a516b1bd6e2767d24cbd360a6943ae988e1669c
Gustavo Faria <gustavoduartefaria@gmail.com>
2019-06-11 17:39:25 +0100

    rcm

--- cache_job.sh
+++ cache_job.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+python -W ignore load_dataset_cache.py VGG16 20 train original
+
+python -W ignore load_dataset_cache.py VGG16 20 train transform
+
+python -W ignore load_dataset_cache.py VGG16 20 train face_patch
+
+python -W ignore load_dataset_cache.py VGG16 20 val original
+
+python -W ignore load_dataset_cache.py VGG16 20 val transform
+
+python -W ignore load_dataset_cache.py VGG16 20 val face_patch
+
+python -W ignore load_dataset_cache.py VGG16 15 train original
+
+python -W ignore load_dataset_cache.py VGG16 15 train transform
+
+python -W ignore load_dataset_cache.py VGG16 15 train face_patch
+
+python -W ignore load_dataset_cache.py VGG16 15 val original
+
+python -W ignore load_dataset_cache.py VGG16 15 val transform
+
+python -W ignore load_dataset_cache.py VGG16 15 val face_patch
+
 python -W ignore load_dataset_cache.py VGG16 0 train original
 
 python -W ignore load_dataset_cache.py VGG16 0 train transform
@@ -35,30 +59,6 @@ python -W ignore load_dataset_cache.py VGG16 10 val original
 python -W ignore load_dataset_cache.py VGG16 10 val transform
 
 python -W ignore load_dataset_cache.py VGG16 10 val face_patch
-
-python -W ignore load_dataset_cache.py VGG16 15 train original
-
-python -W ignore load_dataset_cache.py VGG16 15 train transform
-
-python -W ignore load_dataset_cache.py VGG16 15 train face_patch
-
-python -W ignore load_dataset_cache.py VGG16 15 val original
-
-python -W ignore load_dataset_cache.py VGG16 15 val transform
-
-python -W ignore load_dataset_cache.py VGG16 15 val face_patch
-
-python -W ignore load_dataset_cache.py VGG16 20 train original
-
-python -W ignore load_dataset_cache.py VGG16 20 train transform
-
-python -W ignore load_dataset_cache.py VGG16 20 train face_patch
-
-python -W ignore load_dataset_cache.py VGG16 20 val original
-
-python -W ignore load_dataset_cache.py VGG16 20 val transform
-
-python -W ignore load_dataset_cache.py VGG16 20 val face_patch
 
 python -W ignore load_dataset_cache.py VGG16 25 train original
 
